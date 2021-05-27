524941abea71784ce16eae76f37937d017e3c89e
6872Vishal <77484050+6872Vishal@users.noreply.github.com>
2021-05-28 01:27:00 +0530

    Create Sender.cpp

---
+++ Sender.cpp
@@ -0,0 +1,35 @@
+#include"stream_bms.h"
+
+void decrement_to_zero(unsigned counter)
+{
+  for(int i=counter; i>0; i--)
+  {
+    /*do nothing */
+  }
+  return;
+}
+
+unsigned getdata_from_sensor()
+{
+  return (rand() % 100);
+}
+
+void ToConsole(unsigned output)
+{
+  cout<<output<<"\n";
+}
+struct Datablock *Head = NULL;
+void Transmission_Controller(struct Register RxBuffer_st)
+{
+ 
+  for(int temp=RxBuffer_st.Num_of_Samples;temp>0;temp--)
+  {
+  decrement_to_zero(RxBuffer_st.TimeInterval);
+  struct Datablock* new_node = (struct Node*) malloc(sizeof(struct Datablock));
+   new_node->BMSData = getdata_from_sensor();
+   new_node->next = Head;
+   head = new_node;
+    ToConsole(new_node->BMSData);
+  }
+  
+}
