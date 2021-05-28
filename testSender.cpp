e6323ae30bb9eba6b14c9cf60d049bed0bac83e2
6872Vishal <77484050+6872Vishal@users.noreply.github.com>
2021-05-28 10:59:28 +0530

    Update testSender.cpp

--- testSender.cpp
+++ testSender.cpp
@@ -4,5 +4,9 @@ define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this
 #include "stream_bms.h"
 
 TEST_CASE("infers the breach according to limits") {
+  struct Register sender_buffer_st = {1, 100, 1, 10};
+  struct Datablock* firstblock = NULL;
+  Transmission_Controller(sender_buffer_st, firstblock);
+  REQUIRE(firstblock != NULL);
   
 }
