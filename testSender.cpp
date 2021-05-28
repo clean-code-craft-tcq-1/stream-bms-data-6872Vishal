19f961ec2725a9cb0ec7a4cd746141b637704f29
6872Vishal <77484050+6872Vishal@users.noreply.github.com>
2021-05-28 13:02:40 +0530

    Update testSender.cpp

--- testSender.cpp
+++ testSender.cpp
@@ -3,10 +3,24 @@
 #include "test/catch.hpp"
 #include "stream_bms.h"
 
-TEST_CASE("infers the breach according to limits") {
+TEST_CASE("Test Case 1 : Working condition") {
   struct Register sender_buffer_st = {Start_Tx, 100,BMS_SOC , 10};
   struct Datablock* firstblock = NULL;
   Transmission_Controller(sender_buffer_st, &firstblock);
   REQUIRE(firstblock != NULL);
+  
+}
+
+TEST_CASE("Test Case 2 : Check no of Data Blocks") {
+  int DataBlockCounter = 0;
+  struct Register sender_buffer_st = {Start_Tx, 100,BMS_SOC , 10};
+  struct Datablock* firstblock = NULL;
+  Transmission_Controller(sender_buffer_st, &firstblock);
+  while(firstblock != NULL)
+  {
+    DataBlockCounter++;
+    firstblock = firstblock->next;
+  }
+  REQUIRE(DataBlockCounter == 10);
   
 }
