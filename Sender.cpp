5241b38d02e64302fdae92cdd33e3e2c685a139e
6872Vishal <77484050+6872Vishal@users.noreply.github.com>
2021-05-28 12:30:25 +0530

    Update Sender.cpp

--- Sender.cpp
+++ Sender.cpp
@@ -9,24 +9,37 @@ void decrement_to_zero(unsigned counter)
   return;
 }
 
-unsigned getdata_from_sensor()
+unsigned getdata_from_sensor(enum BMS_Data Sensor)
 {
-  return (rand() % 100);
+  switch(Sensor)
+  {
+    case BMS_SOC:
+      return (rand() % 100);
+      break;
+    case BMS_Temperature:
+      return (rand() % 50);
+      break;
+    case BMS_Data:
+      return(rand() % 25);
+      break;
+    default:
+      return(rand() % 10);
+      break;
+  }
 }
 
 void ToConsole(unsigned output)
 {
   cout<<output<<"\n";
 }
-//struct Datablock *Head = NULL;
+
 void Transmission_Controller(struct Register RxBuffer_st,struct Datablock** Head)
 {
- 
   for(int temp=RxBuffer_st.Num_of_Samples;temp>0;temp--)
   {
   decrement_to_zero(RxBuffer_st.TimeInterval);
   struct Datablock* new_node = (struct Datablock*) malloc(sizeof(struct Datablock));
-   new_node->BMSData = getdata_from_sensor();
+   new_node->BMSData = getdata_from_sensor(RxBuffer_st.DataType);
    new_node->next = *Head;
    *Head = new_node;
     ToConsole(new_node->BMSData);
