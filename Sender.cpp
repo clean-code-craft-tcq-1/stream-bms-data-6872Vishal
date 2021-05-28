b726851b8b12f185b2da3e720a0bf48bad60144b
6872Vishal <77484050+6872Vishal@users.noreply.github.com>
2021-05-28 11:42:49 +0530

    Update Sender.cpp

--- Sender.cpp
+++ Sender.cpp
@@ -19,7 +19,7 @@ void ToConsole(unsigned output)
   cout<<output<<"\n";
 }
 //struct Datablock *Head = NULL;
-void Transmission_Controller(struct Register RxBuffer_st,struct Datablock& Head)
+void Transmission_Controller(struct Register RxBuffer_st,struct Datablock** Head)
 {
  
   for(int temp=RxBuffer_st.Num_of_Samples;temp>0;temp--)
@@ -27,8 +27,8 @@ void Transmission_Controller(struct Register RxBuffer_st,struct Datablock& Head)
   decrement_to_zero(RxBuffer_st.TimeInterval);
   struct Datablock* new_node = (struct Datablock*) malloc(sizeof(struct Datablock));
    new_node->BMSData = getdata_from_sensor();
-   new_node->next = Head;
-   Head = new_node;
+   new_node->next = *Head;
+   *Head = new_node;
     ToConsole(new_node->BMSData);
   }
   
