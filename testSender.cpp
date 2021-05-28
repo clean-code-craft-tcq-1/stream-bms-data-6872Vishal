4a34d0dc47e7b9346836b1d5a989f357cb199586
6872Vishal <77484050+6872Vishal@users.noreply.github.com>
2021-05-28 13:20:54 +0530

    Update testSender.cpp

--- testSender.cpp
+++ testSender.cpp
@@ -6,6 +6,7 @@
 TEST_CASE("Test Case 1 : Working condition") {
   struct Register sender_buffer_st = {Start_Tx, 100,BMS_SOC , 10};
   struct Datablock* firstblock = NULL;
+  cout<<"*********************Test Case : 1********************\n";
   Transmission_Controller(sender_buffer_st, &firstblock);
   REQUIRE(firstblock != NULL);
   
@@ -15,6 +16,7 @@ TEST_CASE("Test Case 2 : Check no of Data Blocks") {
   int DataBlockCounter = 0;
   struct Register sender_buffer_st = {Start_Tx, 100,BMS_SOC , 10};
   struct Datablock* firstblock = NULL;
+  cout<<"*********************Test Case : 2********************\n";
   Transmission_Controller(sender_buffer_st, &firstblock);
   while(firstblock != NULL)
   {
@@ -23,4 +25,15 @@ TEST_CASE("Test Case 2 : Check no of Data Blocks") {
   }
   REQUIRE(DataBlockCounter == 10);
   
+}
+
+TEST_CASE("Test Case 3 : Check Sensor output data") {
+cout<<"*********************Test Case : 3********************\n";
+  int temp;
+  temp = getdata_from_sensor(BMS_Temperature);
+  REQUIRE(temp < 50);
+  temp = getdata_from_sensor(BMS_Data);
+  REQUIRE(temp < 25);
+  temp = getdata_from_sensor(TotalNumber);
+  REQUIRE(temp < 10);  
 }
