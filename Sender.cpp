07b6fd3ea9de35c83249a94214cdade46df35bae
6872Vishal <77484050+6872Vishal@users.noreply.github.com>
2021-06-01 02:47:10 +0530

    Update Sender.cpp

--- Sender.cpp
+++ Sender.cpp
@@ -28,20 +28,9 @@ unsigned getdata_from_sensor(enum BMS_Data Sensor)
   }
 }
 
-void fifofunc(unsigned output1)
-{
-    int fd = open("myfifo", O_WRONLY);
-  if(write(fd, &output1,sizeof(output1)) == -1)
-  {
-    cout<<"failed to write\n";
-  }
-  close(fd);
-}
-
 void ToConsole(unsigned output)
 {
   cout<<output<<"\n";
-
 }
 
 void Transmission_Controller(struct Register RxBuffer_st,struct Datablock** Head)
@@ -55,46 +44,5 @@ void Transmission_Controller(struct Register RxBuffer_st,struct Datablock** Head
    *Head = new_node;
     //ToConsole(new_node->BMSDataSample);
     buffer_output1[temp] = new_node->BMSDataSample;
-  }
-  
+  } 
 }
-/*
-int PipelineSender(void)
-{
-  pid_t childpid;
-  int fd[2];
-  
-  if((pipe(fd) == -1) || ((childpid = fork()) == -1)){
-    perror("Failed to setup pipeline");
-    return 1;
-  }
-  if(childpid == 0)
-  {
-    if(dup2(fd[1],STDOUT_FILENO)== -1)
-    {
-      cout<<"Failed to redirect stdout of sender \n";
-    }
-    else if((close(fd[0]) == -1) || (close(fd[1]) == -1))
-    {
-      cout<<"Failed to close extra pipe descriptors on Sender \n";
-    }
-    else
-    {
-      
-    }
-    return 1;
-  }
-    if(dup2(fd[0],STDIN_FILENO)== -1)
-    {
-      cout<<"Failed to redirect stdin of receiver \n";
-    }
-    else if((close(fd[0]) == -1) || (close(fd[1]) == -1))
-    {
-      cout<<"Failed to close extra pipe descriptors on receiver\n";
-    }
-    else
-    {
-    }
-    return 1;
-}
-*/
